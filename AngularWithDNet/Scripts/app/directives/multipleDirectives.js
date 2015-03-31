7201a4ae1fb87e14329aff69cb7892eee05454e4
Sudheer Reddy <sudheerdesignintegrator@yahoo.in>
2015-03-31 12:10:08 +0530

    Sharing controller using require in directives added

---
+++ AngularWithDNet/Scripts/app/directives/multipleDirectives.js
@@ -0,0 +1,31 @@
+﻿studentsManagement.directive("greeting", function () {
+    return {
+        restrict: "A",
+        template: "<button ng-click='sayHello()' hindi spanish>Say Hello</button>",
+        replace: true,
+        controller: function ($scope) {
+            $scope.greetings = ["hello"];
+            $scope.sayHello = function () {
+                alert($scope.greetings.join());
+            };
+        }
+    }
+}).directive("hindi", function () {
+    return {
+        restrict: "A",
+        priority: 2,
+        require: "^greeting",
+        link: function (scope, element, attr) {
+            scope.greetings.push("नमस्कार");
+        }
+    }
+}).directive("spanish", function () {
+    return {
+        restrict: "A",
+        priority: 1,
+        require: "^greeting",
+        link: function (scope, element, attr) {
+            scope.greetings.push("hola");
+        }
+    }
+});
