67b37e877b721f5814e5509e23b44ae149558ba5
Sudheer Reddy <sudheerdesignintegrator@yahoo.in>
2015-03-31 10:35:00 +0530

    Accessing parent controller methods in isolated scope

--- AngularWithDNet/Scripts/app/controllers/stsInfoCardController.js
+++ AngularWithDNet/Scripts/app/controllers/stsInfoCardController.js
@@ -1,5 +1,11 @@
 ﻿studentsManagement.controller("stsInfoCardController", [
-    "$scope", "$route", function($scope, $route) {
+    "$scope", "$route", "stDataService", function($scope, $route, stDataService) {
         $scope.students = $route.current.locals.students;
+        $scope.removeStudent = function(id, student) {
+            stDataService.remove({ id: id }, function() {
+                alertify.log(student.Name + " is removed.");
+                $scope.students.splice(student, 1);
+            });
+        };
     }
 ]);
--- AngularWithDNet/Scripts/app/directives/stInfoCard.js
+++ AngularWithDNet/Scripts/app/directives/stInfoCard.js
@@ -2,14 +2,6 @@
     return {
         templateUrl: "/templates/StInfoCard.html",
         restrict: "E",
-        scope: {student:"="},
-        controller: function($scope,stDataService) {
-            $scope.removeStudent = function(id,student) {
-                stDataService.remove({ id: id },function() {
-                    alertify.log(student.Name + " is removed.");
-                    $scope.student = null;
-                });
-            };
-        }
+        scope: { student: "=", remove: "&" }
     }
 });
