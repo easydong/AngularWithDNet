a6491b09f910aa0486ea2c6fee1ef9d5267469b5
Sudheer Reddy <sudheerdesignintegrator@yahoo.in>
2015-03-31 10:40:04 +0530

    Deleted unwanted files

--- AngularWithDNet/App_Start/BundleConfig.cs
+++ AngularWithDNet/App_Start/BundleConfig.cs
@@ -28,20 +28,6 @@ namespace AngularWithDNet
             bundles.Add(new ScriptBundle("~/bundles/alertify").Include("~/Scripts/alertify/alertify.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
             bundles.Add(new StyleBundle("~/Content/alertify").Include("~/Content/alertify/alertify.core.css","~/Content/alertify/alertify.default.css"));
         }
